10ee4c7ecd831f8b963bdfaf92b113383f36b3f2
etki <etki@etki.me>
2018-10-03 03:03:35 +0300

    More AsyncTasks helper methods

--- src/main/java/io/ayte/utility/function/AsyncTasks.java
+++ src/main/java/io/ayte/utility/function/AsyncTasks.java
@@ -2,58 +2,80 @@ package io.ayte.utility.function;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
+import java.util.function.Supplier;
 
 public class AsyncTasks {
     private AsyncTasks() {}
-    public static AsyncTask sync(Task<? extends Exception> task) {
-        return new SyncTaskWrapper(task);
+    public static AsyncTask synchronous(Task<? extends Exception> task) {
+        return new SynchronousTaskWrapper(task);
     }
 
     public static AsyncTask fromTaskAndExecutor(Task<? extends Exception> task, Executor executor) {
-        return new ExecutedSyncTask(task, executor);
+        return new ExecutorDelegatedTask(task, executor);
     }
 
     public static AsyncTask from(Task<? extends Exception> task, Executor executor) {
         return fromTaskAndExecutor(task, executor);
     }
 
+    public static Task<Exception> toTask(AsyncTask task) {
+        return new SynchronousExecution(task);
+    }
+
+    /**
+     * Not using {@link Supplier} interface since it is not implied
+     * that there could be any amount of calls made. {@link Callable}
+     * is somewhat that is usually considered for probably being called
+     * only once, something that {@link Task} implies as well.
+     */
     @RequiredArgsConstructor
-    private static class SyncTaskExecution implements Runnable {
+    private static class SyncTaskExecution implements Runnable, Callable<CompletableFuture<Void>> {
         private final Task<? extends Exception> task;
         private final CompletableFuture<Void> result;
 
         @Override
         public void run() {
+            call();
+        }
+
+        @Override
+        public CompletableFuture<Void> call() {
             try {
                 task.execute();
                 result.complete(null);
             } catch (Exception e) {
                 result.completeExceptionally(e);
             }
+            return result;
         }
     }
 
     @RequiredArgsConstructor
-    private static class SyncTaskWrapper implements AsyncTask {
+    private static class SynchronousTaskWrapper implements AsyncTask {
         private final Task<? extends Exception> task;
 
         @Override
         public CompletableFuture<Void> execute() {
-            try {
-                task.execute();
-                return CompletableFuture.completedFuture(null);
-            } catch (Exception e) {
-                CompletableFuture<Void> future = new CompletableFuture<>();
-                future.completeExceptionally(e);
-                return future;
-            }
+            return new SyncTaskExecution(task, new CompletableFuture<>()).call();
         }
     }
 
     @RequiredArgsConstructor
-    private static class ExecutedSyncTask implements AsyncTask {
+    private static class SynchronousExecution implements Task<Exception> {
+        private final AsyncTask task;
+
+        @Override
+        public void execute() throws InterruptedException, ExecutionException {
+            task.execute().get();
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static class ExecutorDelegatedTask implements AsyncTask {
         private final Task<? extends Exception> task;
         private final Executor executor;
 
--- src/main/java/io/ayte/utility/function/Task.java
+++ src/main/java/io/ayte/utility/function/Task.java
@@ -4,6 +4,10 @@ package io.ayte.utility.function;
  * Pretty much {@link Runnable} but with throw support. Acts same as
  * runnable in raw form.
  *
+ * End users should treat tasks as something that can be called at most
+ * once, even if implementation allows calling multiple times at
+ * current moment.
+ *
  * @see Tasks
  *
  * @param <E>
